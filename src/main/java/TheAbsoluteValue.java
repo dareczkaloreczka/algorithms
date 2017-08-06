0a08f5d2374759ce2c0400fb32ca6c17f27ff339
dareczkaloreczka <dk.biernat@gmail.com>
2017-08-06 11:18:19 +0200

    + the absolute value

---
+++ src/main/java/TheAbsoluteValue.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class TheAbsoluteValue {
+
+    private static int absoluteValue (int x){
+        if (x >=0){
+            return  x;
+        } else {
+            return x *= -1;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj x");
+        int x = scanner.nextInt();
+
+        System.out.println(absoluteValue(x));
+
+
+
+
+    }
+}
